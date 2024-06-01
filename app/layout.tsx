733fd3ab9c26484833cb4e0b170dcc676f9e7b75
saumyaa-sharma <saumya.sharma0915@gmail.com>
2024-06-02 00:34:36 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import './globals.css'
+const poppins = Poppins({ 
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins'
+});
+
+export const metadata: Metadata = {
+  title: "Evently",
+  description: "Platofrm for event management",
+  icons:{
+    icon: '/assets//images/logo.svg'
+  }
+};
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
---
+++ lib/database/models/user.models.ts
@@ -0,0 +1,11 @@
+import { Schema, model , models} from "mongoose";
+const UserSchema = new Schema({
+    clerkId: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unqiue: true },
+    username: { type: String, required: true, unique: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    photo: { type: String, required: true },
+})
+const User= models.User ||model('User', UserSchema);
+export default User;

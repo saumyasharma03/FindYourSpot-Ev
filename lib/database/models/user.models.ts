3cd3ff943739a469859b46f0666c8c7fa4c2f309
saumyaa-sharma <saumya.sharma0915@gmail.com>
2024-06-03 09:27:40 +0530

    bugs fixed

--- lib/database/models/user.models.ts
+++ lib/database/models/user.models.ts
@@ -2,7 +2,7 @@ import { Schema, model , models} from "mongoose";
 const UserSchema = new Schema({
     clerkId: { type: String, required: true, unique: true },
     email: { type: String, unqiue: true },
-    username: { type: String, required: true, unique: true },
+    username: { type: String, unique: true },
     firstname: { type: String},
     lastname: { type: String },
     photo: { type: String, required: true },
